6af574da9b5c220e2ca96e63b38302d22ca6b2e1
Ana silva <ana35357.pala@gmail.com>
2026-04-08 22:31:01 -0300

    variaveis

---
+++ fundamentos/Variaveis.js
@@ -0,0 +1,52 @@
+/**
+ * Estudo de variaveis
+ * @author Ana silva 
+ */
+
+console.clear()
+console.log("estudo das variaveis")
+console.log("")
+console.log("string")
+// a linha abaixo cria uma variavel
+let nome = "Ana silva"
+console.log(nome)
+//a linha abaixo mostra um tipo de variavel
+console.log(typeof(nome))
+console.log("")
+console.log("variavel do tipo number")
+let idade = 16
+console.log(idade)
+console.log(typeof(idade))
+let peso = 73
+let altura = 1.64
+console.log("")
+console.log("variavel do tipo boolean")
+let vip = true
+console.log(vip)
+
+
+
+console.log("")
+console.log("-----------------------------------")
+console.log("         Ficha do aluno            ")
+console.log("-----------------------------------")
+// a linha a abaixo concatena (união) um texto com o 
+// conteudo da variavel
+console. log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+let Peso = 73 
+console. log(`peso: ${peso}kg`)
+let Altura = 1.64
+console.log(`altura: ${altura}`)
+console.log (`vip: ${vip}`)
+
+
+
+
+
+
+
+    
+
+
+
